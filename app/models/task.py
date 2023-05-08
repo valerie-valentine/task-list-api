ffda9f1173dff042557a3b76049f94e9129f6a87
Valerie Valentin <valerie.valentine93@gmail.com>
2023-05-08 08:30:39 -0700

    Created Task Model, GET & PUT Routes

--- app/models/task.py
+++ app/models/task.py
@@ -5,4 +5,25 @@ class Task(db.Model):
     task_id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String)
     description = db.Column(db.String)
-    completed_at = db.Column(db.DateTime, nullable = True)
+    completed_at = db.Column(db.DateTime, nullable=True)
+
+
+    def to_dict(self):
+        return dict(
+            id=self.task_id,
+            title=self.title,
+            description=self.description,
+            is_complete=self.completed_at != None)
+
+    @classmethod
+    def from_dict(cls, task_data):
+        new_task = cls(title=task_data["title"],
+                       description=task_data["description"],
+                       completed_at=task_data["completed_at"])
+
+        return new_task
+                
+
+
+
+
