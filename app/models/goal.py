1bd0451a14a32aba39f1b32d65ac26623f1681b9
Valerie Valentin <valerie.valentine93@gmail.com>
2023-05-10 23:29:24 -0700

    Established relationship between goal and task models

--- app/models/goal.py
+++ app/models/goal.py
@@ -4,6 +4,7 @@ from app import db
 class Goal(db.Model):
     goal_id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
+    tasks = db.relationship("Task", back_populates="goal", lazy=True)
 
 
     def to_dict(self):
--- app/models/task.py
+++ app/models/task.py
@@ -7,6 +7,8 @@ class Task(db.Model):
     title = db.Column(db.String)
     description = db.Column(db.String)
     completed_at = db.Column(db.DateTime, nullable=True)
+    goal_id = db.Column(db.Integer, db.ForeignKey('goal.goal_id'), nullable=True)
+    goal = db.relationship("Goal", back_populates="books")
 
 
     def to_dict(self):
