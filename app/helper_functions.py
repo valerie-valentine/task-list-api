1369d1f1a0b76a52309ca10d8049b8ee4ce69bba
Valerie Valentin <valerie.valentine93@gmail.com>
2023-05-11 23:26:24 -0700

    Removed bearer from within api key variable

--- app/helper_functions.py
+++ app/helper_functions.py
@@ -17,13 +17,14 @@ def validate_model(cls, model_id):
 
 def create_slack_message(task):
     api_url = "https://slack.com/api/chat.postMessage"
-
+    TOKEN = os.environ.get("SLACK_API_KEY") 
+                   
     payload = {
     "channel": "#api-test-channel",
     "text": f"Someone just completed the task {task.title}."
     }
     headers = {
-    'Authorization': os.environ.get("SLACK_API_KEY")
+    'Authorization': f"Bearer {TOKEN}"
     }
 
     response = requests.post(api_url, headers=headers, data=payload)
